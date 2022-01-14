b04bde8762fc767b4c47f66e27f2020bb14743fa
PanK0 <panici.k@gmail.com>
2022-01-14 20:53:00 +0100

    Delete JMSStockMarketServant directory

--- JMSStockMarketServant/JMSStockMarketClient/src/it/uniroma1/diag/stockmarket/peer/client/StockMarketClient.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma1.diag.stockmarket.peer.client;
-
-import it.uniroma1.diag.stockmarket.peer.client.gui.AzioniFrame;
-
-public class StockMarketClient {
-	public static void main(String[] args) {
-		new AzioniFrame();
-	}
-}
